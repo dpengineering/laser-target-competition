178f8f1ecc9fa4380787ee1f6a26d55bf9f420d6
hlswan807 <henryswanson@dpengineering.org>
2025-03-13 13:53:11 -0700

    added a InstructionsScreen.kv where the player can enter their name for leaderboards

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ time = time
 screen_manager = ScreenManager()
 target_screen_name = 'target'
 player_screen_name = 'player'
+instructions_screen_name = 'instructions'
 leaderboard = Leaderboard()
 
 
@@ -44,6 +45,24 @@ class LaserTargetCompetitionUI(App):
 
 Window.clearcolor = (0, 0, 0, 1)
 
+class InstructionsScreen(Screen):
+    """
+        Class to handle instructions screen
+    """
+    def __init__(self, **kw):
+        Builder.load_file('InstructionsScreen.kv')
+        super(InstructionsScreen, self).__init__(**kw)
+
+    @staticmethod
+    def transition_to_player_screen():
+        screen_manager.transition.direction = "left"
+        screen_manager.current = player_screen_name
+
+    @staticmethod
+    def transition_to_target_screen():
+        screen_manager.transition.direction = "right"
+        screen_manager.current = target_screen_name
+
 
 class PlayerScreen(Screen):
     """
@@ -53,6 +72,9 @@ class PlayerScreen(Screen):
         super().__init__(**kw)
         self.get_leaderboard()
 
+
+    def on_enter(self, *args):
+        self.get_leaderboard()
 
     def get_leaderboard(self):
         for i, score in enumerate(leaderboard.scores[1]): # 1 is the level
@@ -77,13 +99,29 @@ class PlayerScreen(Screen):
             elif (i+1) == 7:
                 self.ids.name_7.text = str(score['name'])
                 self.ids.score_7.text = str(score['points'])
+            elif (i+1) == 8:
+                self.ids.name_8.text = str(score['name'])
+                self.ids.score_8.text = str(score['points'])
+            elif (i + 1) == 9:
+                self.ids.name_9.text = str(score['name'])
+                self.ids.score_9.text = str(score['points'])
+            elif (i+1) == 10:
+                self.ids.name_10.text = str(score['name'])
+                self.ids.score_10.text = str(score['points'])
             print(str((i + 1)) + ". " + str(score['name']) + " " + str(score['points']))
+
+
 
 
     @staticmethod
     def transition_to_target_screen():
         screen_manager.transition.direction = "left"
         screen_manager.current = target_screen_name
+
+    @staticmethod
+    def transition_to_instructions_screen():
+        screen_manager.transition.direction = "right"
+        screen_manager.current = instructions_screen_name
 
 
 
@@ -106,17 +144,22 @@ class TargetScreen(Screen):
         Builder.load_file('TargetScreen.kv')
         super(TargetScreen, self).__init__(**kwargs)
         self.clock_scheduled = False
-        self.time_start = time_ns()
+        self.time_start = None
         self.time_s = None
+        self.time_ms = None
         self.targets = False
         self.points = None
         self.target_move_to_pedestal_num = 0
         self.state = "idle"
         self.targets_hit = None
         self.off_screen = 800 + 64
-        self.target_quality = {"t1": "rainbow", "t2": "rainbow", "t3": "rainbow", "t4": "rainbow",
-                               "t5": "rainbow", "t6": "rainbow", "t7": "rainbow", "t8": "rainbow",
-                               "t9": "rainbow", "t10": "rainbow"}
+        self.target_quality = {"t1": "prismatic_shard", "t2": "prismatic_shard", "t3": "prismatic_shard", "t4": "prismatic_shard",
+                               "t5": "prismatic_shard", "t6": "prismatic_shard", "t7": "prismatic_shard", "t8": "prismatic_shard",
+                               "t9": "prismatic_shard", "t10": "prismatic_shard"}
+        self.target_move_time = 0
+        self.target_time = 0
+        self.level = 1
+
 
         #unused(for now)
         self.difficulty = "easy"
@@ -132,8 +175,10 @@ class TargetScreen(Screen):
         self.targets = True
         self.target_move_to_pedestal_num = 0
         self.points = 0
-        self.targets_are_in = [False, False, False, False]
-        self.target_hits = [False, False, False, False]
+        self.targets_are_in = [False, False, False, False, False,
+                               False, False, False, False, False]
+        self.target_hits = [False, False, False, False, False,
+                            False, False, False, False, False]
         self.targets_hit = 0
         self.move_targets_offscreen()
 
@@ -142,6 +187,12 @@ class TargetScreen(Screen):
         self.ids.target_2.x = self.off_screen
         self.ids.target_3.x = self.off_screen
         self.ids.target_4.x = self.off_screen
+        self.ids.target_5.x = self.off_screen
+        self.ids.target_6.x = self.off_screen
+        self.ids.target_7.x = self.off_screen
+        self.ids.target_8.x = self.off_screen
+        self.ids.target_9.x = self.off_screen
+        self.ids.target_10.x = self.off_screen
 
     def end(self):
         self.state = "idle"
@@ -150,7 +201,10 @@ class TargetScreen(Screen):
         self.update_time_left_image(15)
 
         if leaderboard.in_top_ten(1, self.points):
+            print("CONGRATS! Your score is on the leaderboard!")
+            self.transition_to_player_screen()
             leaderboard.add_score("HLS", self.points, 1)
+
 
 
 
@@ -158,11 +212,12 @@ class TargetScreen(Screen):
 
     def update_all(self, dt=None): # dt for clock scheduling
         print(f"state={self.state}, target_move_to_pedestal_num={self.target_move_to_pedestal_num}, points={self.points}")
-        self.update_time_left_image(self.update_time_left())
+        self.update_time_left_image(self.update_time())
+        self.update_target_quality()
         if screen_manager.current == target_screen_name:
-            if self.targets and self.targets_hit < 4 and self.time_s > 0:
+            if self.targets and self.targets_hit < 10 and self.time_s > 0:
                 self.move_targets()
-            elif self.time_s == 0 or self.targets_hit == 4:
+            elif self.time_s == 0 or self.targets_hit == 10:
                 self.clock_scheduled = False
                 self.end()
         return self.clock_scheduled
@@ -174,52 +229,75 @@ class TargetScreen(Screen):
         else:
             self.clock_scheduled = False
 
-    def update_time_left(self):
-        #print(f"Updating Label, Time {self.time_s}")
-        self.time_s = -round((time_ns() / 1000000000) - (self.time_start / 1000000000 + 16))
+
+
+    def update_time(self):
+        self.time_s = -round((time_ns() / 1000000000) - (self.time_start / 1000000000 + 15.5)) # seconds counting down from 15 after start has been pressed
+        self.time_ms = round((time_ns() / 1000000) - (self.time_start / 1000000)) # ms since start has been pressed
+        self.target_time = self.time_ms - self.target_move_time
+        print(f"target_move_time={self.target_move_time}, time_ms={self.time_ms}, target_time={self.target_time}")
         return self.time_s
 
-    def update_time_left_image(self, num):
-        #print("updating time left")
-        #print(f"time_left={num}")
-        #please someone make this whole timer system a SelfUpdatingLabel!!!!!!
-        #I don't have the time for that so this will do, but still please
 
-        if num == 0:
-            self.end()
-            self.ids.time_left.text = "00"
-        elif num == 1:
-            self.ids.time_left.text = "01"
-        elif num == 2:
-            self.ids.time_left.text = "02"
-        elif num == 3:
-            self.ids.time_left.text = "03"
-        elif num == 4:
-            self.ids.time_left.text = "04"
-        elif num == 5:
-            self.ids.time_left.text = "05"
-        elif num == 6:
-            self.ids.time_left.text = "06"
-        elif num == 7:
-            self.ids.time_left.text = "07"
-        elif num == 8:
-            self.ids.time_left.text = "08"
-        elif num == 9:
-            self.ids.time_left.text = "09"
-        elif num == 10:
-            self.ids.time_left.text = "10"
-        elif num == 11:
-            self.ids.time_left.text = "11"
-        elif num == 12:
-            self.ids.time_left.text = "12"
-        elif num == 13:
-            self.ids.time_left.text = "13"
-        elif num == 14:
-            self.ids.time_left.text = "14"
-        else:
-            self.ids.time_left.text = "15"
+
+    def update_target_quality(self):
+        quality = "prismatic_shard"
+        if self.target_time > 3000:
+            quality = "gold"
+        elif self.target_time > 1200:
+            quality = "amethyst"
+        elif self.target_time > 600:
+            quality = "emerald"
+        elif self.target_time > 300:
+            quality = "diamond"
+        match self.targets_hit:
+            case 0:
+                self.target_quality['t1'] = quality
+                self.ids.target_1.source = f"assets/images/{quality}_64.png"
+            case 1:
+                self.target_quality['t2'] = quality
+                self.ids.target_2.source = f"assets/images/{quality}_64.png"
+            case 2:
+                self.target_quality['t3'] = quality
+                self.ids.target_3.source = f"assets/images/{quality}_64.png"
+            case 3:
+                self.target_quality['t4'] = quality
+                self.ids.target_4.source = f"assets/images/{quality}_64.png"
+            case 4:
+                self.target_quality['t5'] = quality
+                self.ids.target_5.source = f"assets/images/{quality}_64.png"
+            case 5:
+                self.target_quality['t6'] = quality
+                self.ids.target_6.source = f"assets/images/{quality}_64.png"
+            case 6:
+                self.target_quality['t7'] = quality
+                self.ids.target_7.source = f"assets/images/{quality}_64.png"
+            case 7:
+                self.target_quality['t8'] = quality
+                self.ids.target_8.source = f"assets/images/{quality}_64.png"
+            case 8:
+                self.target_quality['t9'] = quality
+                self.ids.target_9.source = f"assets/images/{quality}_64.png"
+            case 9:
+                self.target_quality['t10'] = quality
+                self.ids.target_10.source = f"assets/images/{quality}_64.png"
+
+        print(f"t1={self.target_quality['t1']}, "
+              f"t2={self.target_quality['t2']}, "
+              f"t3={self.target_quality['t3']}, "
+              f"t4={self.target_quality['t3']}, "
+              f"t5={self.target_quality['t3']}, "
+              f"t6={self.target_quality['t3']}, "
+              f"t7={self.target_quality['t3']}, "
+              f"t8={self.target_quality['t3']}, "
+              f"t9={self.target_quality['t3']}, "
+              f"t10={self.target_quality['t4']}")
+
+
+
 
     def move_specific_target(self, target_num):
+        print(f"{target_num}")
         if self.targets_are_in[target_num - 1]:
             return
         rand_x = round(random() * 800) - 64 # 800 - 64 = screen width - target size
@@ -235,29 +313,101 @@ class TargetScreen(Screen):
             self.ids.target_1.x = rand_x
             self.ids.target_1.y = rand_y
             self.targets_are_in[0] = True
+            self.target_move_time = self.time_ms
         if target_num == 2:
             self.ids.target_2.x = rand_x
             self.ids.target_2.y = rand_y
             self.targets_are_in[1] = True
+            self.target_move_time = self.time_ms
         if target_num == 3:
             self.ids.target_3.x = rand_x
             self.ids.target_3.y = rand_y
             self.targets_are_in[2] = True
+            self.target_move_time = self.time_ms
         if target_num == 4:
             self.ids.target_4.x = rand_x
             self.ids.target_4.y = rand_y
             self.targets_are_in[3] = True
+            self.target_move_time = self.time_ms
+        if target_num == 5:
+            self.ids.target_5.x = rand_x
+            self.ids.target_5.y = rand_y
+            self.targets_are_in[4] = True
+            self.target_move_time = self.time_ms
+        if target_num == 6:
+            self.ids.target_6.x = rand_x
+            self.ids.target_6.y = rand_y
+            self.targets_are_in[5] = True
+            self.target_move_time = self.time_ms
+        if target_num == 7:
+            self.ids.target_7.x = rand_x
+            self.ids.target_7.y = rand_y
+            self.targets_are_in[6] = True
+            self.target_move_time = self.time_ms
+        if target_num == 8:
+            self.ids.target_8.x = rand_x
+            self.ids.target_8.y = rand_y
+            self.targets_are_in[7] = True
+            self.target_move_time = self.time_ms
+        if target_num == 9:
+            self.ids.target_9.x = rand_x
+            self.ids.target_9.y = rand_y
+            self.targets_are_in[8] = True
+            self.target_move_time = self.time_ms
+        if target_num == 10:
+            self.ids.target_10.x = rand_x
+            self.ids.target_10.y = rand_y
+            self.targets_are_in[9] = True
+            self.target_move_time = self.time_ms
+
 
 
     def move_targets(self):
+        num = 0
         if self.clock_scheduled:
-            #print("moving targets")
-            rand = round(random() * 128)
+            if self.level == 1:
+                rand = round(random() * 64)
+                if rand == 1 and not self.target_hits[0] and self.targets_hit == 0:
+                    num = 1
+                if rand == 2 and not self.target_hits[1] and self.targets_hit == 1:
+                    num = 2
+                if rand == 3 and not self.target_hits[2] and self.targets_hit == 2:
+                    num = 3
+                if rand == 4 and not self.target_hits[3] and self.targets_hit == 3:
+                    num = 4
+                if rand == 5 and not self.target_hits[4] and self.targets_hit == 4:
+                    num = 5
+                if rand == 6 and not self.target_hits[5] and self.targets_hit == 5:
+                    num = 6
+                if rand == 7 and not self.target_hits[6] and self.targets_hit == 6:
+                    num = 7
+                if rand == 8 and not self.target_hits[7] and self.targets_hit == 7:
+                    num = 8
+                if rand == 9 and not self.target_hits[8] and self.targets_hit == 8:
+                    num = 9
+                if rand == 10 and not self.target_hits[9] and self.targets_hit == 9:
+                    num = 10
+
+
+            elif self.level == 2:
+                rand = round(random() * 128)
+                if rand == 1 and not self.target_hits[0]:
+                    num = 1
+                if rand == 2 and not self.target_hits[1]:
+                    num = 2
+                if rand == 3 and not self.target_hits[2]:
+                    num = 3
+                if rand == 4 and not self.target_hits[3]:
+                    num = 4
+
+
+            self.move_specific_target(num)
+
 
             #print(f"rand={rand}rand_x={rand_x}rand_y={rand_y}")
 
 
-            # difficulty
+            # difficulty - tutuorial mode
             # easy - one target at a time
             # medium - multiple targets at once, but the same total number of targets as easy
             # hard - multiple targets at once, more targets than easy or medium, and targets spawn faster
@@ -280,18 +430,11 @@ class TargetScreen(Screen):
             # Amethyst - 300p
             # Gold Bar - 100p
 
-            if rand == 1 and not self.target_hits[0]:
-                self.move_specific_target(1)
-            if rand == 2 and not self.target_hits[1]:
-                self.move_specific_target(2)
-            if rand == 3 and not self.target_hits[2]:
-                self.move_specific_target(3)
-            if rand == 4 and not self.target_hits[3]:
-                self.move_specific_target(4)
+
 
     def update_points(self, target):
         match self.target_quality[target]:
-            case "rainbow":
+            case "prismatic_shard":
                 self.points += 1200
             case "diamond":
                 self.points += 900
@@ -352,9 +495,73 @@ class TargetScreen(Screen):
                     self.update_points("t4")
                 case 5:
                     self.target_hits[4] = True
-                    self.ids.target_4.x = pedestal_x
-                    self.ids.target_4.y = pedestal_y
+                    self.ids.target_5.x = pedestal_x
+                    self.ids.target_5.y = pedestal_y
                     self.update_points("t5")
+                case 6:
+                    self.target_hits[5] = True
+                    self.ids.target_6.x = pedestal_x
+                    self.ids.target_6.y = pedestal_y
+                    self.update_points("t6")
+                case 7:
+                    self.target_hits[6] = True
+                    self.ids.target_7.x = pedestal_x
+                    self.ids.target_7.y = pedestal_y
+                    self.update_points("t7")
+                case 8:
+                    self.target_hits[7] = True
+                    self.ids.target_8.x = pedestal_x
+                    self.ids.target_8.y = pedestal_y
+                    self.update_points("t8")
+                case 9:
+                    self.target_hits[8] = True
+                    self.ids.target_9.x = pedestal_x
+                    self.ids.target_9.y = pedestal_y
+                    self.update_points("t9")
+                case 10:
+                    self.target_hits[9] = True
+                    self.ids.target_10.x = pedestal_x
+                    self.ids.target_10.y = pedestal_y
+                    self.update_points("t10")
+
+    def update_time_left_image(self, num):
+        # print("updating time left")
+        # print(f"time_left={num}")
+        # please someone make this whole timer system a SelfUpdatingLabel!!!!!!
+        # I don't have the time for that so this will do, but still please
+
+        if num == 0:
+            self.ids.time_left.text = "00"
+        elif num == 1:
+            self.ids.time_left.text = "01"
+        elif num == 2:
+            self.ids.time_left.text = "02"
+        elif num == 3:
+            self.ids.time_left.text = "03"
+        elif num == 4:
+            self.ids.time_left.text = "04"
+        elif num == 5:
+            self.ids.time_left.text = "05"
+        elif num == 6:
+            self.ids.time_left.text = "06"
+        elif num == 7:
+            self.ids.time_left.text = "07"
+        elif num == 8:
+            self.ids.time_left.text = "08"
+        elif num == 9:
+            self.ids.time_left.text = "09"
+        elif num == 10:
+            self.ids.time_left.text = "10"
+        elif num == 11:
+            self.ids.time_left.text = "11"
+        elif num == 12:
+            self.ids.time_left.text = "12"
+        elif num == 13:
+            self.ids.time_left.text = "13"
+        elif num == 14:
+            self.ids.time_left.text = "14"
+        else:
+            self.ids.time_left.text = "15"
 
     @staticmethod
     def transition_to_player_screen():
@@ -368,6 +575,7 @@ Builder.load_file('main.kv')
 LabelBase.register(name='PixelFont', fn_regular='assets/fonts/Tiny5-Regular.ttf')
 screen_manager.add_widget(PlayerScreen(name=player_screen_name))
 screen_manager.add_widget(TargetScreen(name=target_screen_name))
+screen_manager.add_widget(InstructionsScreen(name=instructions_screen_name))
 
 
 if __name__ == "__main__":
